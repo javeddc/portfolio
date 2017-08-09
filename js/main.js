39c4717c8f7804a2592a50d3429d48f1e674d181
Javed <jdecosta@gmail.com>
2017-08-09 14:04:47 +1000

    jquery src updated

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-
-
-
 $(document).ready(function($) {
   console.log('Hi there!');
   $('#accordion').find('.accordion-toggle').click(function(){
-
-    //Expand or collapse this panel
     $(this).next().slideToggle(400);
-
-    //Hide the other panels
     $(".accordion-content").not($(this).next()).slideUp(400);
-
   });
 });
